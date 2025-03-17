feba975aecabac0015de86d14c74b64a1933ba41
dkmat <dmathur.ce@gmail.com>
2025-03-17 02:11:31 -0700

    started code

--- scratch/lab4-part1.cc
+++ scratch/lab4-part1.cc
@@ -1,18 +1,21 @@
 #include "ns3/internet-stack-helper.h"
 #include "ns3/ipv4-address-helper.h"
-#include "ns3/log.h"
 #include "ns3/mobility-helper.h"
 #include "ns3/mobility-model.h"
 #include "ns3/on-off-helper.h"
 #include "ns3/packet-sink-helper.h"
-#include "ns3/ssid.h"
-#include "ns3/string.h"
-#include "ns3/uinteger.h"
 #include "ns3/yans-wifi-channel.h"
 #include "ns3/yans-wifi-helper.h"
-#include "ns3/command-line.h"
 #include "ns3/config.h"
-
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/spectrum-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include <string>
+#include <iostream>
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("Lab4Part1");
@@ -21,5 +24,22 @@ int
 main(int argc, char *argv[])
 {
     LogComponentEnable("Lab4Part1", LOG_LEVEL_INFO);
-    
+    double exponent = 2.0;
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("exponent", "Path loss exponent for LogDistance model", exponent);
+    cmd.Parse(argc, argv);
+
+    NodeContainer wireStNode, apNode;
+    wireStNode.Create(1);
+    apNode.Create(1);
+
+    SpectrumChannelHelper spectrumChannelHelper = SpectrumChannelHelper::Default();
+    spectrumChannelHelper.AddPropagationLoss("ns3::LogDistancePropagationLossModel", "Exponent", DoubleValue(exponent));
+    Ptr<SpectrumChannel> spectrumChannel = spectrumChannelHelper.Create();
+
+    SpectrumWifiPhyHelper phy = SpectrumWifiPhyHelper::Default();
+    phy.SetChannel(spectrumChannel);
+    phy.Set("ChannelWidth", UintegerValue(20));
+    phy.Set("TxPowerStart", DoubleValue(20.0));
+    phy.Set("TxPowerEnd", DoubleValue(20.0));
 }
