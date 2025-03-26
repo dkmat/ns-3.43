2dfbc5fe14068c2cece3006dbdcdddf51dcf7824
dkmat <dmathur.ce@gmail.com>
2025-03-26 16:55:37 -0400

    working on part 1

--- scratch/lab4-part1.cc
+++ scratch/lab4-part1.cc
@@ -4,8 +4,6 @@
 #include "ns3/mobility-model.h"
 #include "ns3/on-off-helper.h"
 #include "ns3/packet-sink-helper.h"
-#include "ns3/yans-wifi-channel.h"
-#include "ns3/yans-wifi-helper.h"
 #include "ns3/config.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -19,27 +17,74 @@
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("Lab4Part1");
+double interval = 1.0;
 
 int
 main(int argc, char *argv[])
 {
     LogComponentEnable("Lab4Part1", LOG_LEVEL_INFO);
     double exponent = 2.0;
+    
     CommandLine cmd(__FILE__);
     cmd.AddValue("exponent", "Path loss exponent for LogDistance model", exponent);
     cmd.Parse(argc, argv);
 
-    NodeContainer wireStNode, apNode;
-    wireStNode.Create(1);
+    NodeContainer stNode, apNode;
+    stNode.Create(1);
     apNode.Create(1);
 
-    SpectrumChannelHelper spectrumChannelHelper = SpectrumChannelHelper::Default();
+    SpectrumChannelHelper spectrumChannelHelper;
+    spectrumChannelHelper.SetChannel("ns3::MultiModelSpectrumChannel");
     spectrumChannelHelper.AddPropagationLoss("ns3::LogDistancePropagationLossModel", "Exponent", DoubleValue(exponent));
     Ptr<SpectrumChannel> spectrumChannel = spectrumChannelHelper.Create();
 
-    SpectrumWifiPhyHelper phy = SpectrumWifiPhyHelper::Default();
+    SpectrumWifiPhyHelper phy;
     phy.SetChannel(spectrumChannel);
-    phy.Set("ChannelWidth", UintegerValue(20));
+    phy.Set("ChannelSettings",StringValue("{0, 20, BAND_UNSPECIFIED, 0}"));
     phy.Set("TxPowerStart", DoubleValue(20.0));
     phy.Set("TxPowerEnd", DoubleValue(20.0));
+
+    WifiHelper wifi;
+    wifi.SetStandard(WIFI_STANDARD_80211ac);
+    wifi.SetRemoteStationManager("ns3::MinstrelHtWifiManager");
+
+    WifiMacHelper mac;
+    Ssid ssid = Ssid("ns3-80211ac");
+    NetDeviceContainer stDevice, apDevice;
+    mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
+    stDevice = wifi.Install(phy, mac, stNode);
+
+    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
+    apDevice = wifi.Install(phy, mac, apNode);
+
+    MobilityHelper mobility;
+    mobility.SetPositionAllocator("ns3::GridPositionAllocator", "MinX", DoubleValue(0.0), "MinY", DoubleValue(0.0));
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(stNode);
+    mobility.Install(apNode);
+
+    InternetStackHelper stack;
+    stack.Install(stNode);
+    stack.Install(apNode);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer stInterface = address.Assign(stDevice);
+    Ipv4InterfaceContainer apInterface = address.Assign(apDevice);
+
+    uint16_t port = 9;
+    OnOffHelper onoff("ns3::UdpSocketFactory", InetSocketAddress(stInterface.GetAddress(0), port));
+    onoff.SetConstantRate(DataRate("400Mbps"), 1024);
+    ApplicationContainer app = onoff.Install(apNode.Get(0));
+    app.Start(Seconds(1.0));
+    app.Stop(Seconds(10.0));
+
+    PacketSinkHelper sink("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+    ApplicationContainer sinkApp = sink.Install(stNode.Get(0));
+    sinkApp.Start(Seconds(1.0));
+    sinkApp.Stop(Seconds(10.0));
+
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
 }
